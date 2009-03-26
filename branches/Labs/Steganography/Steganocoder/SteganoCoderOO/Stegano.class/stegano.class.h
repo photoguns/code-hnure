404f1faa8a59785e0ccb47a71927608b844d9a41
alexandrovWEB <alexandrovWEB@8592428e-0b6d-11de-9036-69e38a880166>
2009-03-26 19:49:11 +0000

    BLOCK method is done. Started Quantization method developing.

--- branches/Labs/Steganography/Steganocoder/SteganoCoderOO/Stegano.class/stegano.class.h
+++ branches/Labs/Steganography/Steganocoder/SteganoCoderOO/Stegano.class/stegano.class.h
@@ -8,42 +8,34 @@ enum error{WRONG_BMP,TXT_SIZE_ERROR,UNKNOWN_ERROR};
 class Stegano {
 public:
 
-	Stegano(const char *InBMPFilename, const char *InTXTFilename, const char *OutBMPFilename)
-		:InBMPFile(InBMPFilename,std::ios::binary | std::ios::in),
-		InTXTFile(InTXTFilename,std::ios::binary | std::ios::in),
-		OutBMPFile(OutBMPFilename,std::ios::binary | std::ios::out) {
-			inited=0;offset=0;width=0;height=0;capasity=0;messageSize=0;
-	}
-
-	Stegano(const char *InBMPFilename,const char *OutBMPFilename)
-		:InBMPFile(InBMPFilename,std::ios::binary | std::ios::in),
-		OutTXTFile(OutBMPFilename,std::ios::binary | std::ios::out) {
-			inited=0;offset=0;width=0;height=0;capasity=0;messageSize=0;
-	}
+	Stegano():
+	 offset(0),
+	 width(0),
+	 height(0),
+	 capasity(0),
+	 messageSize(0){}
 
 	virtual ~Stegano();
 
-	void encrypt() {
-		init();
+	void encrypt(const char *InBMPFilename, const char *InTXTFilename, const char *OutBMPFilename,  const char *key=0) {
+		init( InBMPFilename, InTXTFilename, OutBMPFilename, 0,  key);
 		enc();
 	}
 
-	void decrypt() {
-		init();
+	void decrypt(const char *InBMPFilename,const char *OutTXTFilename,  const char *key=0) {
+		init( InBMPFilename, 0, 0,OutTXTFilename, key);
 		dec();
 	}
 
 
 private:
-	void init();
-	Stegano(){};
+	void init(const char *,const char *,const char *,const char *,const char *);
 	Stegano(const Stegano&);
 	Stegano& operator=(const Stegano&);
 	virtual void enc()=0;
 	virtual void dec()=0;
 
-protected:
-	bool inited;
+protected:	
 	UINT offset;
 	UINT width;
 	UINT height;
@@ -53,16 +45,6 @@ protected:
 	std::fstream InTXTFile;
 	std::fstream OutBMPFile;
 	std::fstream OutTXTFile;
-};
-
-class withKey {
-protected:
-	withKey(const char *key) {
-		UINT seed=0;
-		for(UINT j=0;j<strlen(key);j++)
-			seed+=key[j];
-		srand(seed);
-	}
 };
 
 #endif
