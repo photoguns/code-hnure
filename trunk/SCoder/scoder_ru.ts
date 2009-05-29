9664c8d59ea8b6fa147dd0c2e41a17f7223728d8
cutwatercore <cutwatercore@8592428e-0b6d-11de-9036-69e38a880166>
2009-05-29 04:16:16 +0000

    Added russian translation file

---
+++ trunk/SCoder/scoder_ru.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_UA">
+<context>
+    <name>ChooseAlgorithmPage</name>
+    <message>
+        <location filename="QtGUI/sources/choosealgorithmpage.cpp" line="15"/>
+        <source>Choose algorithm</source>
+        <translation>Выберите алгоритм</translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/choosealgorithmpage.cpp" line="16"/>
+        <source>
+This one will be used to hide text into container in specific way</source>
+        <translation>Он будет использован чтобы спрятать текст в контейнер с помощью магии</translation>
+    </message>
+</context>
+<context>
+    <name>EnterKeyPage</name>
+    <message>
+        <location filename="QtGUI/sources/enterkeypage.cpp" line="20"/>
+        <source>Enter key</source>
+        <translation>Введите ключ</translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/enterkeypage.cpp" line="21"/>
+        <source>
+The algorithm you have chosen needs key. Please, enter it</source>
+        <translation>Алгоритм который Вы выбрали нуждается в ключе</translation>
+    </message>
+</context>
+<context>
+    <name>EnterTextPage</name>
+    <message>
+        <location filename="QtGUI/sources/entertextpage.cpp" line="19"/>
+        <source>Enter Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/entertextpage.cpp" line="20"/>
+        <source>
+You can type or paste the text which will be injected into container</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HidePage</name>
+    <message>
+        <location filename="QtGUI/sources/hidepage.cpp" line="22"/>
+        <source>Now you can hide it!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/hidepage.cpp" line="23"/>
+        <source>
+Press &apos;Hide&apos; button to inject the text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/hidepage.cpp" line="26"/>
+        <source>&amp;Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageAlgorithm</name>
+    <message>
+        <location filename="QtGUI/sources/imagealgorithm.cpp" line="19"/>
+        <source>&amp;Least Significant Bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/imagealgorithm.cpp" line="20"/>
+        <source>&amp;Pseudo-random interval</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/imagealgorithm.cpp" line="21"/>
+        <source>&amp;Pseudo-random &amp;substitution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/imagealgorithm.cpp" line="22"/>
+        <source>&amp;Blocking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/imagealgorithm.cpp" line="23"/>
+        <source>&amp;Quantization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/imagealgorithm.cpp" line="24"/>
+        <source>&amp;Cross</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/imagealgorithm.cpp" line="25"/>
+        <source>&amp;Koch-Zhao</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IntroPage</name>
+    <message>
+        <location filename="QtGUI/sources/intropage.cpp" line="20"/>
+        <source>Hello user!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/intropage.cpp" line="21"/>
+        <source>
+Please, make your choice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/intropage.cpp" line="24"/>
+        <source>&amp;Hide message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/intropage.cpp" line="25"/>
+        <source>&amp;Get message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpenContainerPage</name>
+    <message>
+        <location filename="QtGUI/sources/opencontainerpage.cpp" line="23"/>
+        <source>Open container</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/opencontainerpage.cpp" line="24"/>
+        <source>You can open a BMP image or a WAV sound file.
+Note: only 24-bit images and 16-bit audio files are supported!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/opencontainerpage.cpp" line="28"/>
+        <source>&amp;Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/opencontainerpage.cpp" line="90"/>
+        <source>Open File...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/opencontainerpage.cpp" line="92"/>
+        <source>Images, Sounds (*.bmp *.wav)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SCoderWizard</name>
+    <message>
+        <location filename="QtGUI/sources/scoderwizard.cpp" line="27"/>
+        <source>SCoder Wizard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SaveContainerPage</name>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="24"/>
+        <source>Save container</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="25"/>
+        <source>
+Chose where to save container after modification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="28"/>
+        <source>&amp;Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="64"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="65"/>
+        <source>Please, specify another path for saving file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="86"/>
+        <source>Images (*.bmp)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="89"/>
+        <source>Sounds (*.wav)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/savecontainerpage.cpp" line="97"/>
+        <source>Save as...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SoundAlgorithm</name>
+    <message>
+        <location filename="QtGUI/sources/soundalgorithm.cpp" line="18"/>
+        <source>&amp;Least Significant Bit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/soundalgorithm.cpp" line="19"/>
+        <source>&amp;Echo</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ViewTextPage</name>
+    <message>
+        <location filename="QtGUI/sources/viewtextpage.cpp" line="19"/>
+        <source>Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtGUI/sources/viewtextpage.cpp" line="20"/>
+        <source>
+Voila! It works, isn&apos;t it?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
