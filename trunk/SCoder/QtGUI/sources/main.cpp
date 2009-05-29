808940d63df759cdb7580ed18578930db49687a5
cutwatercore <cutwatercore@8592428e-0b6d-11de-9036-69e38a880166>
2009-05-29 04:17:53 +0000

    Loading ru translation

--- trunk/SCoder/QtGUI/sources/main.cpp
+++ trunk/SCoder/QtGUI/sources/main.cpp
@@ -1,10 +1,17 @@
 #include <QtGui/QApplication>
+#include <QTranslator>
 #include "scoderwizard.h"
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    translator.load("scoder_ru");
+
+    a.installTranslator(&translator);
+
     SCoderWizard w;
     w.show();
     return a.exec();
